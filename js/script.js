1ffebe41df88f2f107dc0d23281067cb86012abd
Vadim <vadiminsk90@gmail.com>
2021-06-02 10:33:59 +0300

    add image animation

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 'use strict';
 
-const section = document.querySelectorAll('.section');
+const allSections = document.querySelectorAll('.hidden');
 const imgContent = document.querySelectorAll('.section img');
 
 const objOptions = {
   root: null,
   threshold: 0.3,
-  rootMargin: '-100px',
 };
 
 const sectionObserver = new IntersectionObserver(callBackFunction, objOptions);
-sectionObserver.observe(section);
 
-function callBackFunction(entries) {
-  const [entry] = entries;
-  console.log(entry);
-  if (entry.isIntersecting) {
-    imgContent.classList.remove('hidden');
-  } else {
-    imgContent.classList.add('hidden');
+allSections.forEach((section) => {
+  if (section) {
+    sectionObserver.observe(section);
   }
+});
+
+function callBackFunction(entries, observer) {
+  entries.forEach((entry) => {
+    const curSectionsName = entry.target.getAttribute('class');
+    if (entry.isIntersecting) {
+      entry.target.classList.remove('hidden');
+    } else {
+      entry.target.classList.add('hidden');
+    }
+  });
 }
